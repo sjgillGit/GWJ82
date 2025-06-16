7391d2539155f0108b97bd6ba3337c2c248f4aa6
SeanDutt <seanwilcox@comcast.net>
2025-06-16 13:21:53 -0700

    Interaction label
    Added UI node to player, currently only used for interaction label.
    Utilized match case to change text based on what player is looking at. Currently implemented traps, items, and interactibles.
    Implemented a theme, currently only changes label nodes.

---
+++ Scenes/UI/interact_label.gd
@@ -0,0 +1,28 @@
+class_name InteractLabel
+extends Label
+
+@export var player: Player
+
+func _ready():
+	display_label(Item)
+	visible = false
+	player.raycast_hit_changed.connect(display_label)
+
+func display_label(interactible):
+	var key = _get_interact_key()
+	if interactible is Interactible:
+		text = key + " to clean"
+		visible = true
+	elif interactible is Trap:
+		text = key + " to disarm"
+		visible = true
+	elif interactible is Item:
+		text = key + " to pick up"
+		visible = true
+	else:
+		visible = false
+
+
+func _get_interact_key():
+	# There's probably a better way to grab this key
+	return InputMap.action_get_events("grab")[0].as_text()[0]
