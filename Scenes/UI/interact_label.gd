06f5712c3cdcbcfcb96239d8d080432b6917ab7d
SeanDutt <seanwilcox@comcast.net>
2025-06-18 08:37:07 -0700

    Final changes for Phase 2 UI
    Shader applied to player
    Hotbar UI
    Luck temporary UI
    Interaction UI
    Interactible highlighting

--- Scenes/UI/interact_label.gd
+++ Scenes/UI/interact_label.gd
@@ -9,19 +9,25 @@ func _ready():
 
 func display_label(interactible):
 	var key = _get_interact_key()
-	# if interactible is Interactible:
-	# 	text = key + " to clean"
-	# 	visible = true
-	# elif interactible is trap:
-	# 	text = key + " to disarm"
-	# 	visible = true
-	# elif interactible is Item:
-	# 	text = key + " to pick up"
-	# 	visible = true
-	# else:
-	# 	visible = false
+	if interactible is Interactible:
+		text = key + " to clean"
+		visible = true
+	elif interactible is Trap:
+		text = key + " to disarm"
+		visible = true
+	elif interactible is PickableItem:
+		text = key + " to pick up"
+		visible = true
+	else:
+		visible = false
 
 
 func _get_interact_key():
-	# There's probably a better way to grab this key
-	return InputMap.action_get_events("grab")[0].as_text()[0]
+	if (InputMap.has_action("interact")):
+		var key = InputMap.action_get_events("interact")[0].as_text()
+		if key == "Left Mouse Button":
+			return "LMB"
+		return key
+	else:
+		print("No interact action found")
+		return "E"
