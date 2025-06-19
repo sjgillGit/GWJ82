76038d419542ba5bc548ae34f1f373e128b5a5c3
SeanDutt <seanwilcox@comcast.net>
2025-06-19 08:59:12 -0700

    Quick fix for main menu bug
    Options panel on player and main menu now sync up and don't cause a crash.

--- Scenes/UI/setting_toggle.gd
+++ Scenes/UI/setting_toggle.gd
@@ -1,6 +1,7 @@
 class_name SettingToggle extends Control
 
 @export var setting_name: String
+@export var setting_description: String
 @export var setting_default: bool = true
 @export var sub_label: String = "(Disable to improve performance)"
 
@@ -9,16 +10,12 @@ class_name SettingToggle extends Control
 @onready var sub_label_node: Label = %SubLabel
 
 func _ready() -> void:
-    setting_label.text = setting_name
-    setting_toggle.button_pressed = setting_default
-    sub_label_node.text = sub_label
+	setting_label.text = setting_description
+	setting_toggle.button_pressed = VideoSettings[setting_name]
+	sub_label_node.text = sub_label
 
-    setting_toggle.pressed.connect(_on_toggle_pressed)
+	setting_toggle.pressed.connect(_on_toggle_pressed)
 
 func _on_toggle_pressed() -> void:
-    match setting_name:
-        "Dithering":
-            VideoSettings.use_dither = setting_toggle.button_pressed
-        "Quantization":
-            VideoSettings.use_quantize = setting_toggle.button_pressed
-    VideoSettings.apply_visual_settings()
+	VideoSettings[setting_name] = setting_toggle.button_pressed
+	VideoSettings.apply_visual_settings()
