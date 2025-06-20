9e1dab796ad8ddb449a66af9670e3f7642808021
SeanDutt <seanwilcox@comcast.net>
2025-06-20 10:56:16 -0700

    UI Updates
    Set luck container up to handle incoming assets
    Created new theme for project
    Applied theme to main menu and player options panel

--- Scenes/UI/options_panel.gd
+++ Scenes/UI/options_panel.gd
@@ -1,4 +1,4 @@
-class_name OptionsPanel extends Panel
+class_name OptionsPanel extends PanelContainer
 
 @export var is_main_menu : bool = false
 
@@ -6,14 +6,16 @@ class_name OptionsPanel extends Panel
 
 func _ready() -> void:
 	if is_main_menu:
-		back_button.text = "Back"
+		back_button.text = "Close"
 	else:
 		back_button.text = "Back to Main Menu"
 	back_button.pressed.connect(_handle_back_button_pressed)
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
+	if event.is_action_pressed("pause") and !is_main_menu:
 		visible = !visible
+		_set_mouse_mode()
+		
 
 func _handle_back_button_pressed() -> void:
 	visible = false
@@ -21,3 +23,9 @@ func _handle_back_button_pressed() -> void:
 		hide()
 	else:
 		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+func _set_mouse_mode():
+	if visible:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
